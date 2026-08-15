7d09e108aa2f2958f55c973599cd1de6d2d99049
John Kennedy Loria Segundo <jk.1989@hotmail.com>
2026-08-15 01:37:07 -0300

    pay-respects: add fix patch

--- zsh/shared/pay-respects.zsh
+++ zsh/shared/pay-respects.zsh
@@ -2,3 +2,8 @@
 
 # Ativa o pay-respects
 eval "$(pay-respects zsh --alias)"
+
+__pr_base() {
+    prefix=$(print -P "$PROMPT" 2>/dev/null)
+    _PR_MODE="$1" _PR_PREFIX="$prefix" _PR_LAST_COMMAND="$2" _PR_ALIAS="`alias`" _PR_SHELL="zsh" "pay-respects"
+}
